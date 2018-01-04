a86c451d1e73222dd165fdf0b3038d5d8ea8ce31
emoslehi <emoslehi@fs.cs.hm.edu>
2018-01-04 21:58:48 +0100

    Implement search function in AVLTree and some few changes

--- avltree/AVLTree.cpp
+++ avltree/AVLTree.cpp
@@ -1,5 +1,32 @@
-//
-// Created by basti on 12/6/17.
-//
+/**
+ * @authors Ehsan Moslehi, Sebastian Bauman
+ */
 
 #include "AVLTree.h"
+
+using namespace ::std;
+
+AVLTree::Node::Node(const int k) : key(k), bal(0) {}
+
+AVLTree::Node::Node(const int k, Node *l, Node *r)
+        : key(k), left(l), right(r), bal(0) {}
+
+bool AVLTree::search(const int value) const {
+    if (root == nullptr) {
+        return false;
+    }
+    return root->search(value);
+}
+
+bool AVLTree::Node::search(const int value) const {
+    if (value == key) {
+        return true;
+    }
+    if (value < key && left != nullptr) {
+        return left->search(value);
+    }
+    if (value > key && right != nullptr) {
+        return right->search(value);
+    }
+    return false;
+}
